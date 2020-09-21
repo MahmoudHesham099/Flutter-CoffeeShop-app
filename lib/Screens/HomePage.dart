f464eb234e17d565fc47f99195b6436fdca5443c
hesham <17p6020@eng.asu.edu.eg>
2020-09-21 02:25:36 +0200

    coffeeContainer-ui

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterchatbot/Screens/ChatPage.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -9,65 +10,177 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentTab = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-//                boxShadow: [
-//                  BoxShadow(
-//                    color: Colors.grey.withOpacity(0.4),
-//                    spreadRadius: 5,
-//                    blurRadius: 7,
-//                    offset: Offset(0, 3), // changes position of shadow
-//                  ),
-//                ],
-                color: Colors.white,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Order Now',
-                      style: GoogleFonts.lato(
-                        textStyle: TextStyle(
-                          color: Color(0xffE64C3C),
-                          fontSize: 25,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      )),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  AvatarGlow(
-                    endRadius: 40,
-                    glowColor: Colors.red,
-                    duration: Duration(milliseconds: 2000),
-                    repeat: true,
-                    child: GestureDetector(
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Colors.white30,
-                        backgroundImage: AssetImage('assets/chat&order.png'),
+      body: IndexedStack(
+        index: _currentTab,
+        children: <Widget>[
+          Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: GestureDetector(
+                child: Container(
+                  width: 200,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xff79a85a),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 2,
+                        offset: Offset(3, 3), // changes position of shadow
                       ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ChatPage()),
-                        );
-                      },
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          'assets/packet4.png',
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+//                          crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'GUATEMALA',
+                                style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    '\$ 12.00',
+                                    style: GoogleFonts.lato(
+                                      textStyle: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Row(
+//                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.amber,
+                                        size: 20,
+                                      ),
+                                      Text(
+                                        '4.3',
+                                        style: GoogleFonts.lato(
+                                          textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
+                onTap: () {},
               ),
             ),
-          )
+          ),
+          Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(child: Text('profile')),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        height: 80.0,
+        width: 80.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+              elevation: 0.0,
+              backgroundColor: Colors.white30,
+              child: AvatarGlow(
+                endRadius: 40,
+                glowColor: Color(0xff262f56),
+                duration: Duration(milliseconds: 2000),
+                repeat: true,
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white30,
+                  backgroundImage: AssetImage(
+                    'assets/chat-order.png',
+                  ),
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChatPage()),
+                );
+              }),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        currentIndex: _currentTab,
+        onTap: (index) {
+          setState(() {
+            _currentTab = index;
+          });
+        },
+        selectedItemColor: Color(0xff262f56),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 30,
+            ),
+            title: Text(
+              'Home',
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+              size: 30,
+            ),
+            title: Text(
+              'PROFILE',
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
         ],
       ),
     );
